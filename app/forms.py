c3b96bf2e5d3560e4b1743886dada403182c6990
direntwi <yooceeansah@gmail.com>
2022-08-23 22:55:09 +0100

    made changes to pass tests

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from wtforms import (
+    StringField,
+    PasswordField,
+    BooleanField,
+    SubmitField,
+    TextAreaField,
+)
+from wtforms.validators import (
+    ValidationError,
+    DataRequired,
+    Email,
+    EqualTo,
+    Length,
+)
 from app.models import User
 
 
