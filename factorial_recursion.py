3f7114cae520e696dc4fc8402470a0a63349d253
direntwi <yooceeansah@gmail.com>
2022-09-04 15:52:53 +0100

    Added a runtime check for the recursive algorithm

--- factorial_recursion.py
+++ factorial_recursion.py
@@ -1,12 +1,20 @@
 import sys
+import time
 
 sys.setrecursionlimit(1500)
 
-def factorial(n):
+def recursive(n):
     if n == 0:
         return 1
-    return n * factorial(n-1)
+    return n * recursive(n-1)
 
-print(factorial(1000))
+def main():
+    startTime = time.time()
+    ans = recursive(5)
+    endTime = time.time()
+    print ('Answer is ' + str(ans))
+    print('Runime for recursive algorithm is {}'.format(endTime - startTime))
+
+main()
 
 #for some reason the runtime for 1000! is less for this recursion algorithm as compared to the TCO
